998b3d542f63d034d6258eb40246469857001b9b
woskaangel <woskaangel@naver.com>
2020-06-25 14:40:33 +0900

    add modules

---
+++ private/config.js
@@ -0,0 +1,9 @@
+module.exports={
+    'secret':'secretkey',
+    'url':'localhost',
+    'user':'root',
+    'pw':'password',
+    'db':'aibee',
+    "client_id":"884787004521-6pc7r11fbm9f8m5v8htd5nbqenjvvcq0.apps.googleusercontent.com",
+    "client_secret":"nd6yZ8Wmo_XYSxrMRzKHMhDU"
+};
