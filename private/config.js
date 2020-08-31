0d0ac03e9f90fba3ff4cb996b13d832a3e93aa61
woskaangel <woskaangel@naver.com>
2020-08-31 10:10:30 +0900

    Update config.js

--- private/config.js
+++ private/config.js
@@ -1,9 +1,9 @@
 module.exports={
     // DataBase
     "secret":"secretkey",
-    "host":"localhost",
+    "host":"3.17.153.243",
     "user":"root",
-    "pw":"k06120393",
+    "pw":"Kk06120393@",
     "db":"aibee",
     // google
     "client_id":"83424647225-u493u03aagh24m1rqgsk4qokbb04l9cd.apps.googleusercontent.com",
@@ -12,4 +12,4 @@ module.exports={
     "project_id":"bee-assistant-8bd84",
     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2pK4qlxBbjGxU\nd6f/sblHWdLU4JX9uZpKIlOLbx83DG5AJbsnPGV+EVm1jR1QycpOBtE7LPqo+djg\n2cuKERNso+upTHr/nCnw9IXz/1I6Cc7NJnktYoeIRJizBBBrJGgqYJKgUVVPd/g2\nh50IHBMnurpwcBzbOiorPsp1nalgd8qKd/40jZpKEPX9farVAcsta7i9xtcLzaAd\nRk2/QvgoHxTsx9iwm38qt/5nRy403Fw+CatdrGipcMsT6rqgYUIAyt84s81E+/aq\nHJ/iiVv/axVcYaZKZzgZlE/L+RgfY/femIibP8mVDA0vtS4USpbyMT1OPWizWT6h\n4znuZMqNAgMBAAECggEACT3D4jvKhBdS0+NKCNIBQe71j/fdHv1pJR8fgwqYVby3\nR1+hXJbdu8c89saxGQlcxx3pMElSVYJuytG6Tr4WcYTJw8Mu5ggMxzhYHsRocK9W\nW2L69qTpnqd54nTuL72j16ETSjFTU7YKRRgPhf2Fqpj0HgROMXl78Lw4mkLj8xGG\nrliO7usWtLShz/BgTJNgDLXe30hrk8NkhmRlQlpO87nvxLMrQamSNkGZsx8dmhML\nfXRCWxEtPf+DYbNtGZ+7DDzfp7l0sgvxMmG9uhoqpI7aRHK/wnaeRykPDicDxWae\nt0YDzjh8GjzJeKE2nAGkBs1PW2B+hNOMK8MucQlngQKBgQDldpvB9gGVt/xW4d6I\npNm551snMEP2H4UnAIuL8kAs3rOxh8YT0BSeDDv8tr1xycxJRTpiLQ92yUy12gBQ\nHivHkf0UwxeU0n7fgFFkazLSzZtpRhTV5HJzeMpc14gogopOcVo8qsNXIXqLzniL\nbCFVmqtTuhg45PP3uNH+LNIJuQKBgQDLw/A1fbIvM6MYbKsomXwDrfvpKiFiTGa2\nvE+g2vDIWbezyhGFgffpeFyDBgOokBHOYOWI2xwmIJ9/q6X0J6onrOFA/qJdO8fh\nQGsvb1E6jC0FsdwVeida4RJY6/GPO7NcY04d6L5aJ4iSkEbifEqajXX/JF56b27F\n4gPI3RGhdQKBgQCmyI4v7fJ6808Li4LnSQMkN8AufMt5ErIChkfnwX3Z30k3CZv0\nfLLvqa7UYbLTCT1eKF5mS6BI08nZCApgMX3awgb011TFjDZBl5ARcEVxqZaDbzfn\nu/NgcPSNhO9nmnKSz5sqK2huZVwgUEOX+1FDWnebFNFUUT3u2bxwhf1oqQKBgCpM\nDfzMjsRFj8FuLUlMMvYAk54e5Lcm7NtZ052sVPcByKRFecWg6PlJxspb4T6ISWnx\nqzPxZ7JyaylxVA2Ofj+mH0wCj2mTEzaqzJUYImBo4J+h//FlXe37cneK4qW82sp+\nwqRqv7ivJCZoVI6HBmV2n9vKjOIVZ4UkGW0ElrhRAoGALl6xBXMrf/OT3ogruWi3\no1N/3VP82sI2gXkhnGjCLDtYSk01UojEjZlogzU+2lSPNtIfdPEBKP/fPGgt5ms8\n30s2XWYfGX/G0gWSxvef39y0RvqXcPLeDRO9oQ4I/MLbEVYeflOA7JL6LbMQBECb\nRRJ4UIxoITIpXMuJaI0SnHM=\n-----END PRIVATE KEY-----\n",
     "client_email": "bee-assistant-8bd84@appspot.gserviceaccount.com"
-}; 
+};
